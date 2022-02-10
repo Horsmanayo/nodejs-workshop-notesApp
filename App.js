4ce2ed19d19f3ee634855c0bc2a0aa14d0e04dbc
Horsmanayo <oniademola001@gmail.com>
2022-02-10 19:22:07 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+console.log('starting app.js')
+
+const fs = require('fs'); //internal modules
+const yargs = require('yargs'); //third party modules
+const notes = require('./notes.js'); //external modules
+
+const title = yargs.argv.title;
+const body = yargs.argv.body;
+const command = yargs.argv._[0];
+
+if (command === 'add') {
+    console.log('adding note');
+    notes.addingNote(title, body);
+} else if (command === 'remove') {
+    console.log('removing note');
+    notes.removeNote(title);
+} else if (command === 'read') {
+    console.log('reading note');
+    notes.readNote(title);
+} else if (command === 'list') {
+    console.log('listing all note');
+    notes.getAll();
+} else {
+    console.log('Unknown command was used')
+}
